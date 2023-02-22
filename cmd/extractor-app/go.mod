a03c7b41e41db0f1e1d971c3babd0e5872838150
Roger Amorim <rogertc16@gmail.com>
2023-02-22 14:50:37 -0300

    chore: compiling the project

---
+++ cmd/extractor-app/go.mod
@@ -0,0 +1,5 @@
+module extractor-app
+
+go 1.13
+
+require github.com/aws/aws-lambda-go v1.37.0
--- cmd/extractor-app/main.go
+++ cmd/extractor-app/main.go
@@ -1,43 +1,42 @@
 package main
 
 import (
-       "context"
-       "os"
-       "github.com/aws/aws-lambda-go/lambda"
-       "encoding/json"
-       "bytes"
-       "fmt"
-       "net/http"
+    "github.com/aws/aws-lambda-go/lambda"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func HandleRequest(ctx context.Context) {
 
-      url := "https://webhook.site/d2876b61-681d-43cf-8435-00f43cd36203"
-      fmt.Println("HTTP json post:", url)
+	url := "http://ec2-44-211-82-189.compute-1.amazonaws.com/v1/application"
+	fmt.Println("HTTP json post:", url)
 
-      dataMap := map[string]interface{}{
-            "application_owner": os.Getenv("APPLICATION_OWNER"),
-      		"application_name": os.Getenv("APPLICATION_NAME"),
-      		"application_description": os.Getenv("APPLICATION_DESCRIPTION"),
-      		"application_language": os.Getenv("APPLICATION_LANGUAGE"),
-      }
+	dataMap := map[string]interface{}{
+		"application_owner":       os.Getenv("APPLICATION_OWNER"),
+		"application_name":        os.Getenv("APPLICATION_NAME"),
+		"application_description": os.Getenv("APPLICATION_DESCRIPTION"),
+		"application_language":    os.Getenv("APPLICATION_LANGUAGE"),
+	}
 
-      jsonBytes, _ := json.Marshal(dataMap)
-      request, error := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
-      request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	jsonBytes, _ := json.Marshal(dataMap)
+	request, error := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
-      client := &http.Client{}
-      response, error := client.Do(request)
+	client := &http.Client{}
+	response, error := client.Do(request)
 
-      if error != nil {
-      	panic(error)
-      }
+	if error != nil {
+		panic(error)
+	}
 
-      defer response.Body.Close()
-      fmt.Println("response Status:", response.Status)
-
+	defer response.Body.Close()
+	fmt.Println("response Status:", response.Status)
 }
 
 func main() {
-      lambda.Start(HandleRequest)
+	lambda.Start(HandleRequest)
 }
